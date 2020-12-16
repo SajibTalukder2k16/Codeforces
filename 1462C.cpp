1c9e2738a972e62a8dfbbd10afacf2fad4af1b7a
Sajib Talukder <30448179+SajibTalukder2k16@users.noreply.github.com>
2020-12-16 17:43:59 +0600

    Create 1462C.cpp

---
+++ 1462C.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fastread     ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define fileRead freopen("input.txt","r",stdin);
+#define fileWrite freopen("output.txt","w",stdout);
+#define lightoj_test_case_loop(test) for(int cs=1;cs<=test;cs++)
+#define lightoj_output cout<<"Case "<<cs<<": "
+#define pb push_back
+#define mp make_pair
+#define white 0
+#define grey 1
+#define black 2
+#define ll long long
+#define ii_pair pair<int,int>
+int fx[]={+1,-1,0,0};
+int fy[]={0,0,+1,-1};
+int turnOn(int x,int pos) {
+    return x | (1<<pos-1);
+}
+bool isOn(int N,int pos) {
+    return (bool)(N & (1<<pos-1));
+}
+int main()
+{
+    int test;
+    cin>>test;
+    while(test--)
+    {
+        int num;
+        cin>>num;
+        vector<int>vec;
+        if(num>45)
+        {
+            cout<<-1<<endl;
+        }
+        else
+        {
+
+            int now = 9;
+            while(num!=0)
+            {
+                if(num-now>=0)
+                {
+                    vec.push_back(now);
+                    num-=now;
+                }
+                now--;
+                if(now==0 || num==0)
+                    break;
+
+            }
+            int sz=vec.size();
+            for(int i=sz-1;i>=0;i--)
+                cout<<vec[i];
+            cout<<endl;
+        }
+    }
+}
