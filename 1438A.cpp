f2e34155d24e9d142c915f0c81f3fa8684bd46bf
Sajib Talukder <30448179+SajibTalukder2k16@users.noreply.github.com>
2021-02-05 12:36:51 +0600

    Create 1438A.cpp

---
+++ 1438A.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main()
+{
+    int test;
+    cin>>test;
+    while(test--)
+    {
+        int n;
+        cin>>n;
+        for(int i=0;i<n;i++)
+            cout<<1<<" ";
+        cout<<endl;
+    }
+}
