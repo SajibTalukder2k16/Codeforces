f5c385ec4d4845988fac1856f42ca46ec229cfb6
Sajib Talukder <30448179+SajibTalukder2k16@users.noreply.github.com>
2021-03-22 17:24:52 +0600

    Create 1484A.cpp

---
+++ 1484A.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fastread     ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define fileRead freopen("input.txt","r",stdin);
+#define fileWrite freopen("output.txt","w",stdout);
+#define ll long long
+#define mx 100009
+int main()
+{
+    int test;
+    cin>>test;
+    while(test--)
+    {
+        int a,b;
+        cin>>a>>b;
+        cout<<a*b<<endl;
+    }
+}
