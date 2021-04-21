6295953efcd4298d154bf4fafcf765321493c9ca
Sajib Talukder <30448179+SajibTalukder2k16@users.noreply.github.com>
2021-04-21 18:52:17 +0600

    Create 1325B.cpp

---
+++ 1325B.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+#include <unordered_map>
+using namespace std;
+#define fastread     ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define fileRead freopen("input.txt","r",stdin);
+#define fileWrite freopen("output.txt","w",stdout);
+#define ll long long
+#define mp make_pair
+#define mx 1000001
+int main()
+{
+    int test;
+    cin>>test;
+    while(test--)
+    {
+        int n;
+        cin>>n;
+        map<int,bool>mp_ib;
+        int num;
+        int cnt=0;
+        for(int i=0;i<n;i++)
+        {
+            cin>>num;
+            if(mp_ib[num]==false)
+            {
+                cnt++;
+                mp_ib[num]=true;
+            }
+        }
+        cout<<cnt<<endl;
+    }
+}
